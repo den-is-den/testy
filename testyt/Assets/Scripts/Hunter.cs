ac04e4f56e57e8a9d8c533cc21b28edce3515c4d
Aleks226 <bokarev.aleks@mail.ru>
2022-07-24 16:52:31 +0300

    Дрочит на куст

--- testyt/Assets/Scripts/Hunter.cs
+++ testyt/Assets/Scripts/Hunter.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class Hunter : MonoBehaviour
 {
     public float startSpeed = 5f;       //начальная скорость 
+    public float timeSpin = 5f;         //время поиска в кусте 
     private float moveSpeed;            //скорость
     Vector2 movement;                   //вектор координат
 
     public Camera cam;
     private GameObject hider;
+    private GameObject bush;
+
     private Rigidbody2D rb;
     private Animator animator;
     SpriteRenderer sr;
 
     private bool SideTrigger = false;   //флаг на переключение (вправо/влево)
     private bool HiderFind = false;     //флаг на (увидел/не увидел) Hider
+    private bool InBush = false;        //флаг на (находится/не находится) в кусте
 
     void Start()
     {
         moveSpeed = startSpeed;
         hider = GameObject.FindGameObjectWithTag("Hider");
+        bush = GameObject.FindGameObjectWithTag("Bush");
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
@@ -44,22 +49,33 @@ public class Hunter : MonoBehaviour
             HiderFind = false;
 
         if (HiderFind)
-        {
             moveSpeed = Mathf.Lerp(moveSpeed, startSpeed * (2f), 0.006f);
-            Debug.Log(moveSpeed);
-        }
         else
-        {
             moveSpeed = Mathf.Lerp(moveSpeed, startSpeed, 0.006f);
-            Debug.Log(moveSpeed);
+
+        if (Input.GetKeyDown("space") && InBush)
+        {
+            sr.sortingOrder = 1;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            animator.SetBool("Seak", true);
+            Invoke("SeakAnimEnd", timeSpin);
         }
+
     }
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         sr.flipX = SideTrigger == true ? true : false;
-
-
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bush")
+            InBush = true;
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bush")
+            InBush = false;
     }
     private bool IsVisible(Camera camera, GameObject target)
     {
@@ -70,5 +86,12 @@ public class Hunter : MonoBehaviour
             if (plane.GetDistanceToPoint(point) < 0)
                 return false;
         return true;
+    }
+
+    private void SeakAnimEnd()
+    {
+        animator.SetBool("Seak", false);
+        sr.sortingOrder = 0;
+        rb.constraints = RigidbodyConstraints2D.None;
     }
 }
